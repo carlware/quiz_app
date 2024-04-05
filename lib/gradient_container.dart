3d0f65fac8756dc10dbd86d0805966b0b7558bdc
Carlos Ramirez <carlos@laranix.com>
2024-04-05 17:30:32 -0600

    add first screen

---
+++ lib/gradient_container.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(150, 255, 255, 255),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            const Text(
+              "Learn flutter the fun way!",
+              style: TextStyle(
+                color: Color.fromARGB(255, 237, 223, 252),
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.arrow_right_alt_outlined),
+              label: const Text("Start Quiz"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
